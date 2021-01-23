4186d8773e801bc9ce3978a47374e13e1b2bf678
abj489 <77858248+abj489@users.noreply.github.com>
2021-01-23 13:59:47 -0500

    Update ChocoFG.ps1

--- ChocoFG.ps1
+++ ChocoFG.ps1
@@ -4,30 +4,31 @@ $chkEpic = 'C:\Program Files (x86)\Epic Games'
 $chkOrigin = 'C:\Program Files (x86)\Origin\Origin.exe'
 $chkSteam = 'C:\Program Files (x86)\Steam\'
 $chkUplay = 'C:\Program Files (x86)\Ubisoft\'
-$allPKG = $chkDiscord, $chkEpic, $chkOrigin, $chkVPN,$chkSteam ,$chkUplay 
+$allPKG = $chkDiscord, $chkEpic, $chkOrigin, $chkVPN, $chkSteam , $chkUplay 
 $evalPKG = Test-Path $allPKG
 $evalChoco = Test-Path $chkChoco
-$pkgInstall = 'discord', 'epicgameslauncher', 'origin', 'protonvpn','steam', 'uplay'
+$pkgInstall = 'discord', 'epicgameslauncher', 'origin', 'protonvpn', 'steam', 'uplay'
 $i = 0
-
-function Install-Chocolatey {
-  if ($evalChoco -eq $true){
-      Install-Apps
-  }
-  elseif ($evalChoco -eq $false) {
-      Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-  }  
+<#
+.DESCRIPTION
+Searches for applications and if they are not found chocolatey runs and installs them automatically.
+ #>
+if ($evalChoco -eq $true) {
+  Write-Host "Chocolatey is installed." 
+  Install-Apps
 }
+elseif ($evalChoco -eq $false) {
+  Write-Host "Chocolatey is not installed... Running chocolatey install script."
+  Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}  
 
-function Install-Apps {
-  foreach ($pkgName in $pkgInstall){
-    if($pkgName -eq $pkgInstall[$i] -And $evalPKG[$i] -eq $true) {
-      $pkgName+" is installed."
-      $i++
-    }
-    else {
-        choco install $pkgName -y
-        $i++
-    }
+foreach ($pkgName in $pkgInstall) {
+  if ($pkgName -eq $pkgInstall[$i] -And $evalPKG[$i] -eq $true) {
+    $pkgName + " is installed."
+    $i++
+  }
+  else {
+    choco install $pkgName -y
+    $i++
   }
 }
